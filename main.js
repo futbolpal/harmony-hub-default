1c903ad1671ba90760091fbe0f855446fbb6beeb
Jonathan Lewis <futbolpal@gmail.com>
2022-02-12 14:02:50 -0500

    Handle reconnects

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const HarmonyHub = require('harmonyhub-api').HarmonyHub;
+const { HarmonyHub } = require('harmonyhub-api')
 const HUB_HOST = '192.168.1.163';
 const HUB_REMOTE_ID = '17979795';
 const HUB_ACTIVITY_OFF = '-1';
@@ -15,10 +15,17 @@ const run = async () => {
   const activity = await hub.getCurrentActivity();
   const defaultActivity = await getDefaultActivityId();
   console.log("Current activity:", getActivityById(activity).label);
+  hub.on('close', () => {
+    console.log("Reconnecting");
+    hub.connect()
+  });
   hub.on('message', async (message) => {
-    if(message.data && message.data.activityId === HUB_ACTIVITY_OFF) {
-      console.log("Setting activity to Default");
-      await hub.startActivity(defaultActivity);
+    if(message.data && message.data.activityId) {
+      console.log("Activity set to:", getActivityById(message.data.activityId).label);
+      if(message.data.activityId === HUB_ACTIVITY_OFF) {
+        console.log("Setting activity to Default");
+        await hub.startActivity(defaultActivity);
+      }
     }
   })
   if(activity === HUB_ACTIVITY_OFF) {
