9314a3549ab038365ca6a93a4a8c048daa5c6ceb
Jonathan Lewis <futbolpal@gmail.com>
2022-02-12 10:26:05 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const HarmonyHub = require('harmonyhub-api').HarmonyHub;
+const HUB_HOST = '192.168.1.163';
+const HUB_REMOTE_ID = '17979795';
+const HUB_ACTIVITY_OFF = '-1';
+const hub = new HarmonyHub(HUB_HOST, HUB_REMOTE_ID);
+
+
+const run = async () => {
+  const config = await hub.connect();
+  const getDefaultActivityId = async () => config.activity.find((a) => a.label === 'Default').id
+  const getActivityById = (activityId) => config.activity.find((a) => a.id === activityId)
+
+
+  console.log("Hub connected!");
+  const activity = await hub.getCurrentActivity();
+  const defaultActivity = await getDefaultActivityId();
+  console.log("Current activity:", getActivityById(activity).label);
+  hub.on('message', async (message) => {
+    if(message.data && message.data.activityId === HUB_ACTIVITY_OFF) {
+      console.log("Setting activity to Default");
+      await hub.startActivity(defaultActivity);
+    }
+  })
+  if(activity === HUB_ACTIVITY_OFF) {
+    console.log("Setting activity to Default");
+    await hub.startActivity(defaultActivity);
+  }
+}
+
+run();
