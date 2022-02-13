0b9a1794a2cda12ad51414c1b8d3df5aa0173f82
Jonathan Lewis <futbolpal@gmail.com>
2022-02-13 17:27:02 -0500

    Add discovery

---
+++ README.md
@@ -0,0 +1,3 @@
+# Harmony Hub Default
+
+Why - the Harmony Hub remote is useless in PowerOff mode.  None of the remote buttons can be used.  By setting the activity to a default activity, the remote can still be used while off.
--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 #!/usr/bin/env node
 
+const { Explorer } = require('@harmonyhub/discover')
 const { HarmonyHub } = require('harmonyhub-api')
-const HUB_HOST = '192.168.1.163';
-const HUB_REMOTE_ID = '17979795';
+
+const HUB_NAME = process.env.HUB_NAME;
 const HUB_ACTIVITY_OFF = '-1';
-const hub = new HarmonyHub(HUB_HOST, HUB_REMOTE_ID);
 
+const discover = new Explorer(51000);
+discover.on(Explorer.Events.ONLINE, async (discoveredHub) => {
+  console.log('Discovered ' + discoveredHub.friendlyName);
+  if(discoveredHub.friendlyName !== HUB_NAME) {
+    console.log("Skipping ", discoveredHub.friendlyName)
+    return;
+  }
 
-const run = async () => {
+  const { ip, remoteId } = discoveredHub.fullHubInfo
+  const hub = new HarmonyHub(ip, remoteId);
   const config = await hub.connect();
+  console.log("Hub connected!");
+
   const getDefaultActivityId = async () => config.activity.find((a) => a.label === 'Default').id
   const getActivityById = (activityId) => config.activity.find((a) => a.id === activityId)
-
-
-  console.log("Hub connected!");
   const activity = await hub.getCurrentActivity();
   const defaultActivity = await getDefaultActivityId();
+
   console.log("Current activity:", getActivityById(activity).label);
   hub.on('close', () => {
     console.log("Reconnecting");
@@ -34,6 +42,11 @@ const run = async () => {
     console.log("Setting activity to Default");
     await hub.startActivity(defaultActivity);
   }
-}
 
-run();
+});
+
+discover.on(Explorer.Events.OFFLINE, (hub) => {
+  console.log('lost ' + hub.ip);
+});
+
+discover.start();
